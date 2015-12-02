1229d6096a968fd0851540d682f75c493434dd05
Glenn Gersis <glennekegersis@hotmail.com>
2015-12-02 22:39:45 +0100

    User klasse aangepast voor de login aan te maken

--- DeBrabander/Models/User.cs
+++ DeBrabander/Models/User.cs
@@ -9,10 +9,28 @@ namespace DeBrabander.Models
 	public class User
 	{
             [Key]
-            public int ID { get; set; }
-            public string Login { get; set; }
+            public int UserID { get; set; }
+
+            [Required(ErrorMessage ="Voornaam is verplicht")]
+            public string FirstName { get; set; }
+
+            [Required(ErrorMessage = "Familienaam is verplicht")]
+            public string LastName { get; set; }
+
+            [Required(ErrorMessage = "Username is verplicht")]
+            public string Username { get; set; }
+
+            [Required(ErrorMessage = "Email is verplicht")]
+            //validator nog maken
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "Wachtwoord is verplicht")]
+            [DataType(DataType.Password)]
             public string Password { get; set; }
 
+            [Compare("Password", ErrorMessage ="Gelieve je wachtwoord te bevestigen")]
+            [DataType(DataType.Password)]
+            public string ConfirmPassword { get; set; }
             public int Rights { get; set; }
         
     }
