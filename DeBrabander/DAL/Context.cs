c6164e59c51d18fbf36778d648e4598192a52127
Glenn Gersis <glennekegersis@hotmail.com>
2015-12-04 08:14:42 +0100

    Contect naar DbSet<UserAccount> UserAccounts aangepast

--- DeBrabander/DAL/Context.cs
+++ DeBrabander/DAL/Context.cs
@@ -8,23 +8,21 @@ using DeBrabander.Models;
 
 namespace DeBrabander.DAL
 {
-    
-
     public class Context : DbContext
     {
         public Context() : base("DefaultConnection")
         {
         }
 
-        public DbSet<UserAccount> Users { get; set; }
+        public DbSet<UserAccount> UserAccounts { get; set; }
         public DbSet<Customer> Customers { get; set; }
 
         public DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
-            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
             //Tabelnamen worden nu niet meer in meervouden gezet.
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
         }
 
 
