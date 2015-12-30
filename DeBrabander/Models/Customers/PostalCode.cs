b84ebfd86c0b27e948d441046db3f5156cefa0e9
Siferon <brunsontom@gmail.com>
2015-12-30 23:23:19 +0100

    Adding extra files

---
+++ DeBrabander/Models/Customers/PostalCode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DeBrabander.Models
+{
+    public class PostalCode
+    {
+        [Key]
+        [DisplayName("Postcode Id")]
+        public int PostalCodeId { get; set; }
+
+        [DisplayName("Postcode")]
+        public int PostalCodeNumber { get; set; }
+
+        [DisplayName("Gemeente")]
+        public string Town { get; set; }
+    }
+}
---
+++ DeBrabander/Models/Products/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DeBrabander.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [DisplayName("Product Naam")]
+        public string ProductName { get; set; }
+        [DisplayName("Productcode")]
+        public string ProductCode { get; set; }
+        [DisplayName("Opmerking")]
+        public string Remark { get; set; }
+        [DisplayName("Omschrijving")]
+        public string Description { get; set; }
+        [DisplayName("Prijs exclusief BTW")]
+        public double PriceExVAT { get; set; }
+        [DisplayName("Reprobel")]
+        public double Reprobel { get; set; }
+        [DisplayName("BebatT")]
+        public double bebat { get; set; }
+        [DisplayName("Recupel")]
+        public double Recupel { get; set; }
+        [DisplayName("Auvibel")]
+        public double Auvibel { get; set; }
+        [DisplayName("Aankoopprijs")]
+        public double PurchasePrice { get; set; }
+        [DisplayName("Merk")]
+        public string Brand { get; set; }
+        [DisplayName("Categorie")]
+        public int CategoryId { get; set; }
+        [DisplayName("BTW %")]
+        public int VATPercId { get; set; }
+        [DisplayName("Voorraad")]
+        public double Stock { get; set; }
+        [DisplayName("EAN Code")]
+        public string EAN { get; set; }
+
+
+        [DisplayName("Voorraad Controle")]
+        public bool StockControl { get; set; }
+
+        public Category Category { get; set; }
+
+        public VAT VAT { get; set; }
+
+
+
+    }
+}
