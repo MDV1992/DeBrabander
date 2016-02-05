3d37aa405deba5f2b675f0b5eb363e3d733ad4b8
Maarten De Vleeschouwer <maarten.devleeschouwer@gmail.com>
2016-02-05 09:16:03 +0100

    Company settings class

---
+++ DeBrabander/Models/Company/Company.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace DeBrabander.Models.Company
+{
+    public class Company
+    {
+        [Key]
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+        public string District { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; }
+        public string Email {get; set;}
+        public string Country { get; set; }
+        public string VatNumber { get; set; }
+        public string Iban { get; set; }
+        public string BIC { get; set; }
+        public byte LogoLink { get; set; }
+
+
+    }
+}
