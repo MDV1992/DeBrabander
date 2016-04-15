f1a143de050fc0ab3c7f84f82232c9c0594f2c9c
Glenn Gersis <glennekegersis@hotmail.com>
2016-04-15 20:05:27 +0200

    create commit layout

--- DeBrabander/Scripts/TableRowCollapse.js
+++ DeBrabander/Scripts/TableRowCollapse.js
@@ -1,3 +1,11 @@
-﻿$(".toggle-more-detail").on("click", function () {
-    $(this).parent().parent().nextAll('.quotation-detail:first').toggle();
+﻿$(document).ready(function () {
+    $(".select").change(function () {
+        var id = $(this).attr('data-customer');
+        if (this.checked) {
+            $(".rowToExpand." + id).show();
+        } else {
+            $(".rowToExpand." + id).hide();
+        }
+
+    });
 });
