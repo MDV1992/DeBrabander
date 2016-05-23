b6b1314f400881bf0d38a0991e31519d964979e5
Maarten De Vleeschouwer <maarten.devleeschouwer@gmail.com>
2016-05-23 21:32:40 +0200

    Aanpassing settings controller

--- DeBrabander/Controllers/SettingsController.cs
+++ DeBrabander/Controllers/SettingsController.cs
@@ -16,38 +16,10 @@ namespace DeBrabander.Controllers
         public ActionResult Index()
         {
             SettingsViewModel svm = new SettingsViewModel();
-            var categoryList = from c in db.Categories select c;
-            svm.categorys = categoryList.ToList();
+          
             return View(svm);
         }
 
-        //[HttpPost]
-        //public ActionResult UploadImages(HttpPostedFileBase[] uploadImages)
-        //{
-        //    if (uploadImages.Count() <= 1)
-        //    {
-        //        return RedirectToAction("BrowseImages");
-        //    }
-
-        //    foreach (var image in uploadImages)
-        //    {
-        //        if (image.ContentLength > 0)
-        //        {
-        //            byte[] imageData = null;
-        //            using (var binaryReader = new BinaryReader(image.InputStream))
-        //            {
-        //                imageData = binaryReader.ReadBytes(image.ContentLength);
-        //            }
-        //            var headerImage = new HeaderImage
-        //            {
-        //                ImageData = imageData,
-        //                ImageName = image.FileName,
-        //                IsActive = true
-        //            };
-        //            imageRepository.AddHeaderImage(headerImage);
-        //        }
-        //    }
-        //    return RedirectToAction("BrowseImages");
-        //}
+       
     }
 }
